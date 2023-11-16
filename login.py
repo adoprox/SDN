eaa21b57e9af7849d964a765b43a4b92ca01658d
adoprox <112973516+adoprox@users.noreply.github.com>
2023-11-16 11:12:53 +0100

    new file:   customclass.py 	deleted:    flows.py 	deleted:    generated.json 	modified:   login.py 	new file:   mngtopop.py 	new file:   templates/login_1.html

--- login.py
+++ login.py
@@ -1,113 +1,7 @@
-'''
-from flask import Flask, render_template, request, redirect, url_for
-import json
-
-app = Flask(__name__)
-
-# Load existing credentials from a JSON file
-def load_credentials():
-    try:
-        with open('credentials.json', 'r') as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {}
-
-# Save new credentials to the JSON file
-def save_credentials(username, password):
-    credentials = load_credentials()
-    credentials[username] = password
-    with open('credentials.json', 'w') as file:
-        json.dump(credentials, file)
-
-@app.route('/')
-def index():
-    return render_template('login.html')
-
-@app.route('/login', methods=['POST'])
-def login():
-    login_id = request.form.get('loginId')
-    password = request.form.get('password')
-
-    # Validate credentials (replace this with your actual authentication logic)
-    credentials = load_credentials()
-
-    print("Entered Username:", login_id)
-    print("Entered Password:", password)
-    print("Stored Credentials:", credentials)
-
-    if login_id in credentials and credentials[login_id] == password:
-        return f"Login successful for {login_id}!"
-    else:
-        return "Invalid credentials. Please try again."
-
-    # You can redirect to another page after successful login
-    # For now, let's redirect back to the login page
-    return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
-
-"""
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import json
 from werkzeug.security import generate_password_hash, check_password_hash
-
-app = Flask(__name__)
-
-
-# Load existing credentials from a JSON file
-def load_credentials():
-    try:
-        with open('credentials.json', 'r') as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return {}
-
-
-# Save new credentials to the JSON file
-def save_credentials(username, password):
-    credentials = load_credentials()
-    credentials[username] = generate_password_hash(password)
-    with open('credentials.json', 'w') as file:
-        json.dump(credentials, file)
-
-
-@app.route('/')
-def index():
-    return render_template('login.html')
-
-
-@app.route('/login', methods=['POST'])
-def login():
-    login_id = request.form.get('loginId')
-    password = request.form.get('password')
-
-    # Debug: Stampa i valori inseriti nel form
-    print("Entered Username:", login_id)
-    print("Entered Password:", password)
-
-    # Validate credentials (replace this with your actual authentication logic)
-    credentials = load_credentials()
-    print("Stored Credentials:", credentials)
-
-    if login_id in credentials and check_password_hash(credentials[login_id], password):
-        return f"Login successful for {login_id}!"
-    else:
-        return "Invalid credentials. Please try again."
-
-    # Codice inutile dopo il return, verrà ignorato
-    return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""
-from flask import Flask, render_template, request, redirect, url_for
-import json
-from werkzeug.security import generate_password_hash, check_password_hash
+from mngtopo import flag_hosts, flow_rules
 
 app = Flask(__name__)
 
@@ -125,14 +19,18 @@ def save_credentials(username, password):
         json.dump(credentials, file)
 
 # Aggiungi queste linee per creare le credenziali iniziali
-save_credentials("user1", "ciao")
-save_credentials("user2", "bella")
-save_credentials("user3", "no")
+save_credentials("10.0.0.1", "host1")
+save_credentials("10.0.0.2", "host2")
+save_credentials("10.0.0.3", "host3")
+save_credentials("10.0.0.4", "host4")
+save_credentials("10.0.0.5", "host5")
+save_credentials("10.0.0.6", "host6")
+save_credentials("10.0.0.7", "host7")
 
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('login.html', authenticated_users=[], error_message=None)
 
 
 @app.route('/login', methods=['POST'])
@@ -149,15 +47,32 @@ def login():
         print("Stored Password Hash:", stored_password_hash)
 
         if stored_password_hash and check_password_hash(stored_password_hash, password):
-            return f"Login successful for {login_id}!"
+            host_data = flag_hosts(login_id)
+            if host_data:
+                host_data["flag"] = "1"
+                print(host_data)
+
+                deviceID = host_data["elementId"]
+                response = flow_rules(deviceID)
+
+                if response.status_code == 201:
+                    print("Request successful")
+                    #print(response)
+                else:
+                    print(f"Request failed with status code: {response.status_code}")
+                #call a function to change the flow rules here, pass the login_id. 
+
+                authenticated_users = [{"username": login_id, "password": password}]
+                return render_template('login.html', authenticated_users=authenticated_users, error_message=None)
         else:
-            return "Invalid credentials. Please try again."
+            error_message = "Invalid credentials. Please try again."
+            return render_template('login.html', authenticated_users=[], error_message=error_message)
 
     except Exception as e:
         print(f"An error occurred: {e}")
-        return "An error occurred during login. Please try again."
-
-
+        error_message = "An error occurred during login. Please try again."
+        return render_template('login.html', authenticated_users=[], error_message=error_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
